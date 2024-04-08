0d4b2fefa5f2ed73277913918eabedc9b75e800a
Sminnitex <77793527+Sminnitex@users.noreply.github.com>
2024-04-08 13:13:56 +0200

    first_results

--- plot.py
+++ plot.py
@@ -2,7 +2,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 df = pd.read_csv("output/time.csv")
-gmean= df.mean()
-gstd = df.std()
-gmean.plot(yerr=gstd,ylabel="time (s)", xlabel="log2(n)")
+gmean= df["Time"]
+gstd = df["Dimensions"]
+plt.figure()
+plt.plot(gstd, gmean)
+plt.xlabel("Matrix n*n dimension")
+plt.ylabel("Time (s)")
+plt.title("Normal matrix transpose")
+plt.xlim(513, 1024)
+plt.ylim(0, 1)
 plt.show()
--- transpose.c
+++ transpose.c
@@ -9,64 +9,65 @@ int main(int argc, char *argv[]){
     //Initialize all the stuff we need
     srand(time(NULL));
     int power = strtol(argv[1], NULL, 10);
-    long arrive = pow(2, power);
-    int block = arrive / 64;
-    //long long number = arrive-10 << arrive;
-    long number = arrive;
+    long number = pow(2, power);
+    int block = number / 64;
     //printf("block size = %d \n", block);
 
-    float **matrix = matrixInitialize(number, number);
-
-    //Assign random values to matrices
-    for (int i = 0; i < number; i++){
-        for (int j = 0; j < number; j++){
-            matrix[i][j] = randomf();
-        }
+    FILE *csvTime = fopen("output/time.csv", "w");
+    if (csvTime == NULL) {
+        printf("Error opening file!\n");
+        return 1;
     }
 
-    
-    float **transposeBlock = matrixInitialize(number, number);
-    float **transpose = matrixInitialize(number, number);
+    fprintf(csvTime, "Time,Dimensions\n");
 
-    //check validity
-    if (matrix == NULL || transposeBlock == NULL || transpose == NULL){
-        return 1; // ERROR: malloc did not work
-    }
-
-    long long tries = 1 << 10;
+    long long tries = 1 << 9;
     double time, time2;
 
     //tries loop
     for (int count = 0; count < tries; count++){
+        float **matrix = matrixInitialize(number, number);
+
+        //Assign random values to matrices
+        for (int i = 0; i < number; i++){
+            for (int j = 0; j < number; j++){
+                matrix[i][j] = randomf();
+            }
+        }
+        
+        float **transposeBlock = matrixInitialize(number, number);
+        float **transpose = matrixInitialize(number, number);
+
+        //check validity
+        if (matrix == NULL || transposeBlock == NULL || transpose == NULL){
+            return 1; // ERROR: malloc did not work
+        }
         //Matrix block transpose
-        clock_t begin = clock();
-        transposeBlockMatrix(matrix, transposeBlock, number, number, block);
-        clock_t end = clock();
-        time = (double) (end-begin) / CLOCKS_PER_SEC;
-        printf("%f\n", time); 
+        //clock_t begin = clock();
+        //transposeBlockMatrix(matrix, transposeBlock, number, number, block);
+        //clock_t end = clock();
+        //time = (double) (end-begin) / CLOCKS_PER_SEC;
+        //fprintf(csvTime, "%f,%ld\n", time, number); 
 
         //Matrix normal transpose
-        //clock_t begin2 = clock();
-        //transposeMatrix(matrix, transpose, number, number);
-        //clock_t end2 = clock();
-        //time2 = (double) (end2-begin2) / CLOCKS_PER_SEC;
-        //printf("%f\n", time2);  
+        clock_t begin2 = clock();
+        transposeMatrix(matrix, transpose, number, number);
+        clock_t end2 = clock();
+        time2 = (double) (end2-begin2) / CLOCKS_PER_SEC;
+        fprintf(csvTime, "%f,%ld\n", time2, number); 
+
+        //Lines for debug purposes
+        //printMatrix(matrix, number, number);
+        //printMatrix(transposeBlock, number, number);
+        //printMatrix(transpose, number, number);
+
+        matrixDestroyer(matrix, number);
+        matrixDestroyer(transposeBlock, number);
+        matrixDestroyer(transpose, number);
+        number = number - 1;
     }      
 
-    //Lines for debug purposes
-    //printMatrix(matrix, number, number);
-    //printMatrix(transposeBlock, number, number);
-    //printMatrix(transpose, number, number);
-
-    //Let's close everything
-    
-
-    //int memory_usage = 3 * number * number * sizeof(float);
-    //printf("Memory usage = %d bytes\n", memory_usage);
-
-    matrixDestroyer(matrix, number);
-    matrixDestroyer(transposeBlock, number);
-    matrixDestroyer(transpose, number);
+    fclose(csvTime);
 
     return 0;
 }
